31842069d5e425a6c4d36cd1fac872328dd3ef1f
Bekithemba <putthetrust.gamedev@gmail.com>
2023-06-10 11:37:47 +0200

    add: custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "dark-slate-grey": "hsl(234, 29%, 20%)",
+        "charcoal-grey": "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)",
       },
     },
   },
   plugins: [],
-}
+};
